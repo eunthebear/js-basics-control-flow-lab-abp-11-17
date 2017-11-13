5ad3221f15ce2ce8392dedaea11bec611a375494
eunthebear <eun.david@gmail.com>
2017-11-13 00:46:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function scuberGreetingForFeet(distance) {
 }
 
 function ternaryCheckCity(city) {
-  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go';
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip(tip) {
